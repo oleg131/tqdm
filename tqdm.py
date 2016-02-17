a03830e990d548464561daf995128839ba423125
Oleg Kim <olegk@sclgroup.cc>
2016-02-17 23:26:04 +0000

    better pandas and ipythonnb compatibility

--- tqdm.py
+++ tqdm.py
@@ -1,4 +1,4 @@
-__all__ = ['tqdm', 'trange']
+__all__ = ['tqdm']
 
 import sys
 import time
@@ -19,24 +19,24 @@ def format_meter(n, total, elapsed):
     # elapsed - number of seconds passed since start
     if n > total:
         total = None
-    
+
     elapsed_str = format_interval(elapsed)
     rate = '%5.2f' % (n / elapsed) if elapsed else '?'
-    
+
     if total:
         frac = float(n) / total
-        
+
         N_BARS = 10
         bar_length = int(frac*N_BARS)
         bar = '#'*bar_length + '-'*(N_BARS-bar_length)
-        
+
         percentage = '%3d%%' % (frac * 100)
-        
+
         left_str = format_interval(elapsed / n * (total-n)) if n else '?'
-        
+
         return '|%s| %d/%d %s [elapsed: %s left: %s, %s iters/sec]' % (
             bar, n, total, percentage, elapsed_str, left_str, rate)
-    
+
     else:
         return '%d [elapsed: %s, %s iters/sec]' % (n, elapsed_str, rate)
 
@@ -45,11 +45,15 @@ class StatusPrinter(object):
     def __init__(self, file):
         self.file = file
         self.last_printed_len = 0
-    
+
     def print_status(self, s):
         self.file.write('\r'+s+' '*max(self.last_printed_len-len(s), 0))
         self.file.flush()
         self.last_printed_len = len(s)
+
+    def clear(self):
+        self.file.write('\r')
+        self.file.flush()
 
 
 def tqdm(iterable, desc='', total=None, leave=False, file=sys.stderr,
@@ -58,27 +62,36 @@ def tqdm(iterable, desc='', total=None, leave=False, file=sys.stderr,
     Get an iterable object, and return an iterator which acts exactly like the
     iterable, but prints a progress meter and updates it every time a value is
     requested.
+
     'desc' can contain a short string, describing the progress, that is added
     in the beginning of the line.
+
     'total' can give the number of expected iterations. If not given,
     len(iterable) is used if it is defined.
+
     'file' can be a file-like object to output the progress message to.
     If leave is False, tqdm deletes its traces from screen after it has
     finished iterating over all elements.
+
     If less than mininterval seconds or miniters iterations have passed since
     the last progress meter update, it is not updated again.
     """
+
     if total is None:
         try:
             total = len(iterable)
-        except TypeError:
-            total = None
-    
+        except:
+            try:
+                iterable = list(iterable)
+                total = len(iterable)
+            except TypeError:
+                total = None
+
     prefix = desc+': ' if desc else ''
-    
+
     sp = StatusPrinter(file)
     sp.print_status(prefix + format_meter(0, total, 0))
-    
+
     start_t = last_print_t = time.time()
     last_print_n = 0
     n = 0
@@ -93,22 +106,12 @@ def tqdm(iterable, desc='', total=None, leave=False, file=sys.stderr,
                 sp.print_status(prefix + format_meter(n, total, cur_t-start_t))
                 last_print_n = n
                 last_print_t = cur_t
-    
+
     if not leave:
-        sp.print_status('')
+        sp.clear()
         sys.stdout.write('\r')
     else:
         if last_print_n < n:
             cur_t = time.time()
             sp.print_status(prefix + format_meter(n, total, cur_t-start_t))
         file.write('\n')
-
-
-def trange(*args, **kwargs):
-    """A shortcut for writing tqdm(range()) on py3 or tqdm(xrange()) on py2"""
-    try:
-        f = xrange
-    except NameError:
-        f = range
-    
-    return tqdm(f(*args), **kwargs)
